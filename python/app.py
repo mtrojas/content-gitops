76e1535450a48e511c5a29922d21621298aab01a
MT <mtrojas@protonmail.com>
2021-11-29 14:03:00 +0000

    feat: change message for MT

--- python/app.py
+++ python/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello Dear MT! I see you preparing for your new job at Pento, GO FOR IT!"
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host="0.0.0.0", port=8000)
